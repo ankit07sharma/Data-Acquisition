0d26eb8af186f0d8a3ead6d63f660df8f870e137
Ankit sharma <82817419+ankit07sharma@users.noreply.github.com>
2021-10-31 13:59:53 +0530

    Update uartDriver.c

--- uartDriver.c
+++ uartDriver.c
@@ -1,9 +1,15 @@
+
+//UART will send data of sensor from microcontroller to serial terminal on system
+
 #include<reg51.h>
 #include"header.h"
+
+
+//This function will set the buadrate and initiliaze the UART to use it
 void UART_INIT(u16 baud)
 {	
-	SCON=0x50;
-	TMOD|=0x20;
+	SCON=0x50;                                               //it will set the UART in universal mode
+	TMOD|=0x20;                                              //Timer1 should be in MODE2
 	switch(baud)
 	{
 		case 7200:TH1=252;break;
@@ -13,24 +19,33 @@ void UART_INIT(u16 baud)
 		case 57600:TH1=255;PCON|=0x80;break;
 		default:TH1=253;
 	}
-	TR1=1;//start baud setting
+	TR1=1;                                                  //start baud setting
 }
+
+
+//This Function will transmit the one byte
 void uart_tx(u8 d)
 {
 	SBUF=d;
 	while(TI==0);
 	TI=0;
 }
+
+
+//This Function will transmit the string
 void uart_tx_string(u8 *arr)
 {
-		while(*arr!=0)
-		{
-			SBUF=*arr;
-			while(TI==0);
-			TI=0;
-			arr++;
-		}
+	while(*arr!=0)
+	{
+	        SBUF=*arr;
+	 	while(TI==0);
+		TI=0;
+		arr++;
 	}
+}
+
+
+//This Function will reverse the string
 void my_strrev(u8 *s)
 {
 	u8 *t=s,i,l=0,temp;
@@ -47,6 +62,8 @@ void my_strrev(u8 *s)
 	}
 }
 	
+
+//This Function will transmit integer number
 void uart_tx_int(s16 t)
 {
 	u8 s[7],i=0;
@@ -67,6 +84,10 @@ void uart_tx_int(s16 t)
 	else
 		uart_tx('0');
 }
+
+
+
+//This Function will transmit the float number 
 void uart_tx_float(float t)
 {
 float temp;
