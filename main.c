c3999bb6e880f06b42cb842f722ce49431a50978
Ankit sharma <82817419+ankit07sharma@users.noreply.github.com>
2021-11-08 20:15:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 #include"header.h"
 void main()
 {
-u16 buf,buf2,speed;
-//float t;
+s16 buf,speed;
+u16 buf2;
+float t;
 UART_INIT(9600);
 RTC_TIMESET();
 while (1)
@@ -12,25 +13,37 @@ while (1)
 uart_tx_string("\n\r");
 RTC_CONTEXT();
 uart_tx_string("\n\r"); 
-buf=ADC_READING(1,0);
-buf2=ADC_READING(0,0); 
-buf2=(buf2*5.0)/4095;
-speed=ADC_READING(0,1);
-speed=(speed*120)/4095;
+  
+  
 uart_tx_string("LDR SENSOR OUTPUT\n\r");
 uart_tx_string("Light intensity-");
-buf=(buf/4095.0)*100;
-uart_tx_int(buf);
-uart_tx('%');
-uart_tx_string("\n\r\n");
+buf=ADC_READING(1,0);  
+buf=(buf/4095.0)*100;  
+uart_tx_int(100-buf);
+uart_tx_string("%\n\r\n");
+delayms(100);
+
+  
 uart_tx_string("Output in voltage\n\r");
-uart_tx_string("Voltage:-");
-uart_tx_float(buf2);
+uart_tx_string("Voltage:-");  
+buf2=ADC_READING(0,0); 
+if(buf2!=0)
+{
+  t=(buf2*5.0)/4095;
+  uart_tx_float(t);
+}
+else
+uart_tx_string("0.0");   
 uart_tx_string("\n\r\n");
+delayms(100);  
+  
+  
 uart_tx_string("Speed of Car\n\r");
-uart_tx_string("Speed:-");
+uart_tx_string("Speed:-");  
+speed=ADC_READING(0,1);
+speed=(speed*120.0)/4095;
 uart_tx_int(speed);
 uart_tx_string ("K/hr\n\r"); 
-delayms(200);
-}
+delayms(400);
+}  
 }
